13e39f357a045ab7a842e8f502bea782e7cd6fe3
raulcf <raulcf@uchicago.edu>
2020-07-28 16:59:06 -0500

    tweaks to iface

--- src/main/java/xsystem/XSystem.java
+++ src/main/java/xsystem/XSystem.java
@@ -1,11 +1,16 @@
 package xsystem;
 
 import java.util.Iterator;
+import java.util.List;
 import java.util.regex.Pattern;
 
 public interface XSystem {
 
     XStructure build(Iterator<String> data);
+
+    Iterator<String> generate(XStructure pattern);
+
+    List<String> generate(XStructure pattern, int limit);
 
     boolean match(XStructure pattern, String value);
 
