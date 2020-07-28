000c6978df8ee68ab0a511943769f4f0f8eda34b
raulcf <raulcf@uchicago.edu>
2020-07-28 16:53:01 -0500

    XSystem iface proposal -- basic version

--- src/main/java/xsystem/XSystem.java
+++ src/main/java/xsystem/XSystem.java
@@ -1,4 +1,16 @@
 package xsystem;
 
+import java.util.Iterator;
+import java.util.regex.Pattern;
+
 public interface XSystem {
+
+    XStructure build(Iterator<String> data);
+
+    boolean match(XStructure pattern, String value);
+
+    boolean match(XStructure pattern1, XStructure pattern2);
+
+    boolean match(XStructure pattern1, Pattern regex);
+
 }
